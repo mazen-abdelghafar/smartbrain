d6a3090bc5dedaa9a54c693ab5338c6578012f8e
Mazen Mohamed <kidkever@yahoo.com>
2020-12-11 06:24:09 +0200

    changing api url

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input }, () => {
-      fetch("http://localhost:3001/imageurl", {
+      fetch("https://peaceful-refuge-50521.herokuapp.com/imageurl", {
         method: "post",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({
@@ -81,7 +81,7 @@ class App extends Component {
         .then((response) => {
           this.displayFaceBox(this.calculateFaceLocation(response));
 
-          fetch("http://localhost:3001/image", {
+          fetch("https://peaceful-refuge-50521.herokuapp.com/image", {
             method: "put",
             headers: { "content-type": "application/json" },
             body: JSON.stringify({
